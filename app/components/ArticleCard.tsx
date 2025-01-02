435024208d88c98740e270b1ce5ac7ff72b4d3d5
Eriuden <antoinecarrette@hotmail.fr>
2025-01-02 15:23:33 +0100

    build: Database 1

--- app/components/ArticleCard.tsx
+++ app/components/ArticleCard.tsx
@@ -1,7 +1,14 @@
 export const ArticleCard = ({article} : {article: articleCardType}) => {
+  const {img, name, typeArticle, groupe, price} = article
   return (
     <li>
-
+        <div className="flex-between">
+            <img src={img}/>          
+            <h2>{name}</h2>
+            <h2>{typeArticle}</h2>
+            <h2>{groupe}</h2>
+            <h2>{price}</h2>    
+        </div>
     </li>
   )
 }
