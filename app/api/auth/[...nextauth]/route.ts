bbf03aebf896821d761592762290856c01ce17de
Eriuden <antoinecarrette@hotmail.fr>
2025-01-03 13:43:52 +0100

    API setup 5

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,8 @@
 import { handlers } from "@/auth" // Referring to the auth.ts we just created
 export const { GET, POST } = handlers
+import articleModel from "../../../server/mongoDB/models/article.model"
+
+export const handler = async(req: any,res :any) => {
+    const readArticle = await articleModel.find({})
+    res.status(200).json(readArticle)
+}
