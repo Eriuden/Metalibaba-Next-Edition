df658c6498d89f803a2e1b7e62fb345e8635e13f
Eriuden <antoinecarrette@hotmail.fr>
2025-01-05 18:15:55 +0100

    API setup 5

--- app/api/auth/[...nextauth]/articleRoute.ts
+++ app/api/auth/[...nextauth]/articleRoute.ts
@@ -1,5 +1,5 @@
 import {likeArticle,dislikeArticle, readArticle, 
-createArticle, updateArticle, deleteArticle}
+createArticle, updateArticle, deleteArticle, unlikeArticle, undislikeArticle}
 from "../../../server/mongoDB/controller/article.controller"
 
 
@@ -15,10 +15,24 @@ export const handler = async(req: any,res :any) => {
         case "PUT":
             updateArticle(req,res)
         case "DELETE":
-            deleteArticle(req,res)     
+            deleteArticle(req,res)
+        case "PATCH":
+            if (action ==="like"){
+                likeArticle(req,res)
+            } else if (action ==="dislike") {
+                dislikeArticle(req,res)
+            } else if (action ==="dislike") {
+                dislikeArticle(req,res) 
+            } else if (action ==="unlike") {
+                unlikeArticle(req,res)}
+              else if (action ==="undislike") {
+                undislikeArticle(req,res)
+            } else {
+                return res.status(400).json({ error: "Unknown action"})
+            }
             
         default:
-            res.setHeader("Allow", ["GET", "POST", "PUT", "DELETE"])
+            res.setHeader("Allow", ["GET", "POST", "PUT", "DELETE","PATCH"])
             res.status(405).end(`Method ${method} Not Allowed`)
     }
 }
